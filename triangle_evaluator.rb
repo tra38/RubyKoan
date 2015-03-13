b00d81cdd6c9feb70bd56358b96222896d45695e
tra38 <tra38@users.noreply.github.com>
2015-03-13 15:42:17 -0500

    Update and rename triangle.rb to triangle_evaluator.rb

--- triangle_evaluator.rb
+++ triangle_evaluator.rb
@@ -4,3 +4,4 @@ return :equilateral if a==b && b==c
 return :isosceles  if (a==b || b==c || c==a)
 return :scalene
 end
+##Return can serve to terminate ("break") a method early! This reduces the amount of code you need to use for this method.
