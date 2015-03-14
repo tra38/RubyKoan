c62d76efc2c41acdccf818a87ff508dcb1101dd2
tra38 <tra38@users.noreply.github.com>
2015-03-14 12:48:33 -0500

    Refactor the code to remove extraenous lines
    Was inspired by https://gist.github.com/mjansen401/840634 to reduce the amount of code needed for checking all the "ones" and "fives" that are rolled. Now I will need to comment the code so that I remember what it does in 6 months.

--- greed.rb
+++ greed.rb
@@ -7,14 +7,12 @@ def greed
   score = 0
   for i in 1..6
     if i == 1
-      score += 1000  if dice.count(i) >= 3
+      score += (1000 + ((dice.count(i) - 3) * 100) ) if dice.count(i) >= 3
       score += 100*dice.count(i) if dice.count(i) >= 1 && dice.count(i) < 3
-      score += 100*(dice.count(i) - 3) if dice.count(i) >= 4
       next
     elsif i == 5 
-      score +=500 if dice.count(i) >= 3
+      score += (500 + 50*(dice.count(i) - 3) )  if dice.count(i) >= 3
       score += 50*dice.count(i)  if dice.count(i) >= 1 && dice.count(i) < 3
-      score += 50*(dice.count(i) - 3) if dice.count(i) >= 4
       next
     else
       score += 100*i if dice.count(i) >= 3
