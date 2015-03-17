b00bf05ab7e9dc7e5ae61c377e7cfce732006173
tra38 <tra38@users.noreply.github.com>
2015-03-16 21:25:37 -0500

    Displays "point categories" to player
    Player now knows how they gained points.

--- greed.rb
+++ greed.rb
@@ -11,29 +11,39 @@ def greed
       #if you rolled 3 or more ones, get 1000 points for rolling 3 ones plus 100 points for every additional one
       if dice.count(i) >= 3
         score += (1000 + ((dice.count(i) - 3) * 100) )
-        print "I rolled triple Ones!"
+        puts "Triple 1s: 1000"
+        if dice.count(i) > 3
+          puts "Additional #{i}s: #{(dice.count(i) - 3) * 100}"
+          end
         end
       #if you rolled less than 3 ones, just get 100 points for every one you rolled
       if dice.count(i) >= 1 && dice.count(i) < 3
        score += 100*dice.count(i)
-       puts "I rolled single Ones!"
+       puts "Lone #{i}s: #{100*dice.count(i)}"
        end
+    next
     end
     if i == 5 #if you roll a five
       #if you rolled 3 or more fives, get 500 points for rolling 3 fives plus 50 points for every additional five
       if dice.count(i) >= 3
         score += (500 + 50*(dice.count(i) - 3) )
-        puts "I rolled triple Fives!"
+        puts "Triple 5s: 50"
+        if dice.count(i) > 3
+          puts "Additional 5s: #{(dice.count(i) - 3) * 50}"
+          end
         end
       #if you rolled less than 3 fives, just get 50 points for every five you rolled
       if dice.count(i) >= 1 && dice.count(i) < 3
         score += 50*dice.count(i)
-        puts "I rolled single Fives!"
+        puts "Lone 5s: #{50*dice.count(i)}"
         end
-      end
+    next
+    end
     #get 100*i points if you roll a number other than one or five 3 times or more
-    score += 100*i if dice.count(i) >= 3
-    puts "I rolled triple #{i}!" if dice.count(i) >= 3
+    if dice.count(i) >= 3
+      score += 100*i if dice.count(i) >= 3
+      puts "Triple #{i}s: #{100*i}"
+    end
   end
   #report your score to the player
   puts "Your score is #{score}. Thanks for playing!"
